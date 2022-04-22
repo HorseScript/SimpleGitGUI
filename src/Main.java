ac98d972e40953ee6ee57e98404914016c40dd4a
gemsvido <gemsvido@gmail.com>
2022-04-22 14:52:36 -0700

    new

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,34 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        System.out.println("\n\n---------- START SIMPLEGITGUI ----------\n\n");
+
+
         try {
 
             if (args[0].equals("--pull-success") || args[0].equals("-pls")) {
                 PullSuccess.create();
+            } else if (args[0].equals("--push-success") || args[0].equals("-phs")) {
+                PushSuccess.create();
             } else {
-                System.out.println("SimpleGitGUI: Args are invalid.\n");
+                System.out.println("\n\nSimpleGitGUI: Args are invalid.\n");
                 System.out.println("Valid args: \n--pull-success\n-pls");
             }
         } catch (Exception e) {
             System.out.println("SimpleGitGUI: No args provided.\n");
-            System.out.println("Valid args: \n--pull-success\n-pls");
+            System.out.println("Valid args: \n\n--pull-success\n-pls\n\n--push-success\n-phs");
         }
 
+        end();
 
 
+    }
+
+    public static void end () {
+
+        System.out.println("\n\n---------- END SIMPLEGITGUI ----------\n\n");
+        System.exit(0);
 
     }
 
--- src/PushSuccess.java
+++ src/PushSuccess.java
@@ -1,14 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 
-public final class PullSuccess {
+public final class PushSuccess {
 
     private JButton OKButton;
     private JPanel panel;
 
 
-
-    private PullSuccess() {
+    private PushSuccess() {
 
         OKButton.addActionListener(e -> {
 
@@ -27,7 +26,7 @@ public final class PullSuccess {
     public static void create () {
         JFrame frame = new JFrame("PullSuccess");
         frame.setPreferredSize(new Dimension(400, 200));
-        frame.setContentPane(new PullSuccess().panel);
+        frame.setContentPane(new PushSuccess().panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
